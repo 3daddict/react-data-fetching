4cf7becbc4085ba43bbbb0029a91b813453f1ba3
Michael Salvati <print@3daddict.com>
2019-01-28 14:04:16 -0800

    search query call created and wired to component

--- src/App.js
+++ src/App.js
@@ -25,6 +25,18 @@ export default class App extends Component {
     });
   }
 
+  performSearch = (query) => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+    .then((response) => {
+      this.setState({
+        gifs: response.data.data
+      })
+    })
+    .catch((error) => {
+      console.log('Error fetching and parsing data', error);
+    });
+  }
+
   render() { 
     console.log(this.state.gifs);
     return (
@@ -32,7 +44,7 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
+            <SearchForm onSearch={this.performSearch} />      
           </div>   
         </div>    
         <div className="main-content">
