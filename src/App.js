03af59968bc6bec7b2ba1c14be9a0227ac73894f
System Administrator <root@Michaels-MacBook-Pro.local>
2019-01-28 13:42:10 -0800

    axios call created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
@@ -13,14 +14,15 @@ export default class App extends Component {
   } 
 
   componentDidMount() {
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({ gifs: responseData.data });
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    .then((response) => {
+      this.setState({
+        gifs: response.data.data
       })
-      .catch(error => {
-        console.log('Error fetching and parsing data', error);
-      });
+    })
+    .catch((error) => {
+      console.log('Error fetching and parsing data', error);
+    });
   }
 
   render() { 
