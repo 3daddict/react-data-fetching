b4fcdd084d440aeefd7e78bf2b2227b55e74f7c7
System Administrator <root@Michaels-MacBook-Pro.local>
2019-01-28 13:56:33 -0800

    displaying data on dom with mapped data

--- src/Components/GifList.js
+++ src/Components/GifList.js
@@ -3,9 +3,14 @@ import Gif from './Gif';
 
 const GifList = props => { 
   
+  const results = props.data;
+  let gifs = results.map(gif =>
+    <Gif url={gif.images.fixed_height.url} key={gif.id} />
+  );
+  
   return(
     <ul className="gif-list">
-      <Gif />
+      {gifs}
     </ul> 
   );
 }
